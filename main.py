42dbb4c768442f97b3645146982ff9c99f5e00c3
jsvaldezv <jsvaldezv@gmail.com>
2021-10-23 19:38:13 -0500

    Fix Pan Pistas

--- main.py
+++ main.py
@@ -2,11 +2,8 @@ import clingo
 import random
 import math
 import loadTracks
+import copy
 import soundfile as sf
-
-def calculteFactor(inPan):
-    factor = inPan / 10
-    return factor
 
 clingo_args = [ "--warn=none",
                 "--sign-def=rnd",
@@ -17,7 +14,7 @@ clingo_args = [ "--warn=none",
                 "--enum-mode=record"]
 
 # **** NUMERO DE MEZCLAS POR HACER ***** #
-numMixes = 1
+numMixes = 10
 
 # **** CONFIGURAR Y CARGAR CLINGO ***** #
 control = clingo.Control(clingo_args)
@@ -26,7 +23,7 @@ control.load("mixer.lp")
 models = []
 
 # **** INSTRUMENTOS A MEZCLAR ***** #
-instrumentos = ["kick", "over", "guitOne", "bass", "guitTwo", "tomOne", "tomTwo", "tomThree"]
+instrumentos = ["kick", "guitOne", "guitTwo", "tomOne"]
 
 # *** CARGAR AUDIOS **** #
 loadedTracks = loadTracks.loadTracks(instrumentos)
@@ -43,16 +40,18 @@ control.ground([("base", [])])
 print("------")
 
 # **** SOLVE ***** #
+print("Solving...")
 with control.solve(yield_=True) as solve_handle:
     for model in solve_handle:
         models.append(model.symbols(shown=True))
+print("------")
 
 # **** OBTENER RESULTADOS ***** #
-cont = 1
-
+cont = 0
 resultados = []
 for model in models:
-    print("MIX ", cont)
+    resp = []
+    print("MIX ", cont+1)
     for atom in model:
         #print(atom)
 
@@ -65,10 +64,12 @@ for model in models:
         resul.append(pan)
         # resul.append(vol)
 
-        resultados.append(resul)
-        print("APLICAR", pan, "de paneo a", instrumento)
-        # print("APLICAR", pan, "de paneo a", instrumento, "con un volumen de", vol)
+        resp.append(resul)
 
+        print("APLICAR", pan, "de paneo a", instrumento)
+        # print("Aplicar", pan, "de paneo a", instrumento, "con un volumen de", vol)
+
+    resultados.append(resp)
     cont += 1
 
 print("------")
@@ -78,26 +79,41 @@ resultados = sorted(resultados)
 loadedTracks = sorted(loadedTracks)
 
 # HACER OPERACIONES DE PANEO #
-cont = 0
-trackFinal = 0
+tracksFinales = []
 
-for track in resultados:
-    # PANEO
-    factor = calculteFactor(track[1])
-    left_factor = math.cos(3.141592 * (factor + 1) / 4)
-    right_factor = math.sin(3.141592 * (factor + 1) / 4)
+for answer in range(numMixes):
 
-    # VOLUMEN
-    #vol = track[2]
+    if (answer+1) <= len(resultados):
+        tracksModified = copy.deepcopy(loadedTracks)
+        trackFinal = 0
+        cont = 0
 
-    # OPERACIONES CON TRACKS
-    loadedTracks[cont][1][:, 0] *= left_factor
-    loadedTracks[cont][1][:, 1] *= right_factor
+        for track in resultados[answer]:
+            # PANEO
+            factor = track[1] / 10
+            left_factor = math.cos(3.141592 * (factor + 1) / 4)
+            right_factor = math.sin(3.141592 * (factor + 1) / 4)
 
-    # SUMAR TRACKS
-    trackFinal += loadedTracks[cont][1]
-    cont += 1
+            # VOLUMEN
+            #vol = track[2]
 
-# CREAR AUDIOS
-for mixes in range(numMixes):
-    sf.write('mixes/mix_' + str(mixes) + '.wav', trackFinal, 44100, 'PCM_24')
+            # OPERACIONES CON TRACKS
+            tracksModified[cont][1][:, 0] *= left_factor
+            tracksModified[cont][1][:, 1] *= right_factor
+
+            # SUMAR TRACKS
+            trackFinal += tracksModified[cont][1]
+
+            cont += 1
+
+        sf.write('mixes/mix_' + str(answer+1) + '.wav', trackFinal, 44100, 'PCM_24')
+    else:
+        print("Ya no hay más mezclas disponibles")
+
+
+
+'''textfile = open('mixes/dato' + str(answer) + str(cont) + '.txt', "w")
+for elem in tracksModified[cont][1]:
+    textfile.write(str(elem[0]) + ', ' + str(elem[1]) + '\n')
+    #print("elem:", elem)
+textfile.close()'''
